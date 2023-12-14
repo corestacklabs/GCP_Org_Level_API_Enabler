e0e0c074d3d38869133e2c9bf1bb251c1b8753fe
Rishi Agrawal <rishi.agrawal@corestack.io>
2023-12-14 11:24:45 +0530

    fix 3

--- run.sh
+++ run.sh
@@ -14,9 +14,9 @@ else
     exit 0 
   elif [ "${INPUT}" == "Yes" ]||[ "${INPUT}" == "yes" ] ; then   
     read -p "Please enter the Organisation id: " org_id
-    cat <<EOF > ./vars.tfvars
-    org_id = "$org_id"
-    api = ["cloudresourcemanager.googleapis.com" ,"compute.googleapis.com","recommender.googleapis.com", "securitycenter.googleapis.com", "orgpolicy.googleapis.com", "sqladmin.googleapis.com", "monitoring.googleapis.com", "pubsub.googleapis.com"]
+cat <<EOF > ./vars.tfvars
+org_id = "$org_id"
+api = ["cloudresourcemanager.googleapis.com" ,"compute.googleapis.com","recommender.googleapis.com", "securitycenter.googleapis.com", "orgpolicy.googleapis.com", "sqladmin.googleapis.com", "monitoring.googleapis.com", "pubsub.googleapis.com"]
 EOF
     terraform init
     terraform apply -var-file="vars.tfvars" -auto-approve
