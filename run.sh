f40dbe95f879a47d9484723c8eca51b6d10ddcfb
Rishi Agrawal <rishi.agrawal@corestack.io>
2023-12-14 11:49:30 +0530

    fix

--- run.sh
+++ run.sh
@@ -1,26 +1,9 @@
-#!/bin/bash
-echo "Please ensure you have the necessary IAM permissions .The execution of this script requires following user account permissions: "
-echo "- Organization level Security Admin Role"
-echo "- Service Usage Admin"
-read -p "Please specify yes if above pre-requisites met or specify no : " INPUT
-if [ -f vars.tfvars ]; then
-  echo vars.tfvars exists.
-  terraform init
-  terraform apply -var-file="vars.tfvars" -auto-approve
-  echo "Approved"
-else
-  if [ "${INPUT}" == "No" ] || [ "${INPUT}" == "no" ] ; then
-    echo 'Exiting..'
-    exit 0 
-    fi
-  elif [ "${INPUT}" == "Yes" ]||[ "${INPUT}" == "yes" ] ; then   
-    read -p "Please enter the Organisation id: " org_id
+#!/bin/bash 
+read -p "Please enter the Organisation id: " org_id
 cat <<EOF > ./vars.tfvars
 org_id = "$org_id"
 api = ["cloudresourcemanager.googleapis.com" ,"compute.googleapis.com","recommender.googleapis.com", "securitycenter.googleapis.com", "orgpolicy.googleapis.com", "sqladmin.googleapis.com", "monitoring.googleapis.com", "pubsub.googleapis.com"]
 EOF
-    terraform init
-    terraform apply -var-file="vars.tfvars" -auto-approve
-    echo "Approved"
-  fi
-fi
+terraform init
+terraform apply -var-file="vars.tfvars" -auto-approve
+echo "Approved"
