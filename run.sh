70b9f1847fabd62468d887604bfbd578d46f7723
Rishi Agrawal <rishi.agrawal@corestack.io>
2023-12-14 11:32:12 +0530

    changes

--- run.sh
+++ run.sh
@@ -1,13 +1,8 @@
-#!/bin/bash
-echo "Please ensure you have the necessary IAM permissions .The execution of this script requires following user account permissions: "
-echo "- Organization level Security Admin Role"
-echo "- Service Usage Admin" 
-read -p "Please enter the Organisation id: " org_id
 cat <<EOF > ./vars.tfvars
 org_id = "$org_id"
 api = ["cloudresourcemanager.googleapis.com" ,"compute.googleapis.com","recommender.googleapis.com", "securitycenter.googleapis.com", "orgpolicy.googleapis.com", "sqladmin.googleapis.com", "monitoring.googleapis.com", "pubsub.googleapis.com"]
 EOF
+
 terraform init
 terraform apply -var-file="vars.tfvars" -auto-approve
 echo "Approved"
-
